a113ef26b0d5f0f16924525297535fa671482c45
arikui1911 <arikui.ruby@gmail.com>
2015-11-24 00:49:45 +0900

    fix code header

--- src/token.c
+++ src/token.c
@@ -14,36 +14,10 @@
 # include <stdlib.h>
 #endif /* HAS_STDLIB */
 
-#include "common.h"
-#include "yacc.h"
-
-#include "token.h"
-#include "misc.h"
-
-
-#ifndef global
-/* return values of function token_get */
-#define	NAME		'a'
-#define	NUMBER		'0'
-#define SPACE		' '
-#define NEWLINE		'\n'
-#define	MARK		0x100
-#define	BEGININC	0x101
-#define	ENDINC		0x102
-#define	TOKEN		0x103
-#define	LEFT		0x104
-#define	RIGHT		0x105
-#define	NONASSOC	0x106
-#define	PRECTOK		0x107
-#define	TYPE		0x108
-#define	UNION		0x109
-#define	START		0x10a
-#define COMMENT		0x10b
-
-#define EXPECT		0x10c
-#define PURE_PARSER	0x10d
-
-#endif /* global */
+#include "./common.h"
+#include "./yacc.h"
+#include "./token.h"
+#include "./misc.h"
 
 static int get(void);
 static void unget(int);
